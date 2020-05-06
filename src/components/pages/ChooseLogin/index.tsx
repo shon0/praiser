ac5b206901cc506646a9c2764963b4d461dce252
ShonoKoga <shono.koga@gmail.com>
2020-05-06 17:36:53 +0900

    Add routing

---
+++ src/components/pages/ChooseLogin/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const Component = () => {
+  return (
+    <View style={styles.container}>
+      <Text>>Choose Login</Text>
+    </View>
+  );
+};
+
+export default Component;
