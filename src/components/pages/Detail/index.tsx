ddd1539c94c608ee8e733b5df81fbf8adfb4103a
ShonoKoga <shono.koga@gmail.com>
2020-05-12 10:10:16 +0900

    Add Input and Detail

--- src/components/pages/Detail/index.tsx
+++ src/components/pages/Detail/index.tsx
@@ -1,18 +1,74 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { useRoute, RouteProp } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import { Button, TextField } from '../../atoms';
+import { useControlledComponent } from '../../../lib/hooks';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 10,
+  },
+  textField: {
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 20,
   },
 });
 
+type TodoEditActions = {
+  changeTodo: (
+    id: string,
+    newValue: {
+      title: string;
+      detail: string;
+    },
+  ) => void;
+};
+
+type Params = {
+  id: string;
+  isEditable: boolean;
+  title: string;
+  detail: string;
+};
+
 const Component = () => {
+  const { goBack } = useNavigation();
+  const { params } = useRoute<RouteProp<Record<string, Params>, string>>();
+  const {
+    isEditable,
+    title: titleInitialValue,
+    detail: detailInitialValue,
+  } = params;
+  const title = useControlledComponent(titleInitialValue);
+  const detail = useControlledComponent(detailInitialValue);
+
+  const onSubmit = React.useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
     <View style={styles.container}>
-      <Text>Detail</Text>
+      <TextField
+        disabled={!isEditable}
+        label="title"
+        value={title.value}
+        onChangeText={title.onChangeText}
+        style={styles.textField}
+      />
+      <TextField
+        disabled={!isEditable}
+        label="detail"
+        value={detail.value}
+        onChangeText={detail.onChangeText}
+        style={styles.textField}
+      />
+      {isEditable && (
+        <Button onPress={onSubmit} label="Submit" style={styles.button} />
+      )}
     </View>
   );
 };
--- src/components/pages/Input/index.tsx
+++ src/components/pages/Input/index.tsx
@@ -1,19 +1,81 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import { useNavigation } from '@react-navigation/native';
+import { TextField, Button, dismiss, IconButton } from '../../atoms';
+import { COLOR } from '../../../constants/theme';
+import { useControlledComponent } from '../../../lib/hooks';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
     justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: COLOR.MAIN,
+  },
+  text: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 20,
+  },
+  iconButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 80,
+    height: 80,
+    borderRadius: 40,
   },
 });
 
 const Component = () => {
+  const title = useControlledComponent('');
+  const detail = useControlledComponent('');
+
+  const { goBack } = useNavigation();
+  const back = React.useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  const addTodo = React.useCallback(() => {
+    back();
+    title.onChangeText('');
+    detail.onChangeText('');
+  }, [back, title, detail]);
+
   return (
-    <View style={styles.container}>
-      <Text>Input</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={dismiss}>
+        <View style={styles.container}>
+          <IconButton
+            icon="close"
+            size={30}
+            iconColor={COLOR.PRIMARY}
+            onPress={back}
+            style={styles.iconButton}
+          />
+          <TextField
+            label="Title"
+            value={title.value}
+            onChangeText={title.onChangeText}
+            style={styles.text}
+          />
+          <TextField
+            label="Detail"
+            value={detail.value}
+            onChangeText={detail.onChangeText}
+            style={styles.text}
+          />
+          <Button
+            onPress={addTodo}
+            label="Add"
+            style={styles.button}
+            disabled={!title.value}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 };
 
